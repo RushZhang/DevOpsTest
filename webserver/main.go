9c501ea1cac73f98ce3b889e88a78b0fabe5b68f
Rush <rush.ruixuan@gmail.com>
2018-08-04 14:14:28 +0800

    first page

---
+++ webserver/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func firstPage(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Hello, I am Rush Zhang</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", firstPage)
+	http.ListenAndServe(":7000", nil)
+}
